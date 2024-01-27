1d6888170c51340f2ee2c228885a1fbd94731e8d
Łukasz Pękalski <lukasz.pekalski@outlook.com>
2024-01-27 19:21:10 +0100

    Update 02_installPrep.sh

--- 02_installPrep.sh
+++ 02_installPrep.sh
@@ -7,8 +7,8 @@ sgdisk -og $disk
 sgdisk -n 1:0:+$EFIsize -n 2:0:$swapSize -n 3:0:+$sysSize -n 4:0:0 $disk -t 1:ef00
 mkfs.vfat -F 32 -n EFI $disk"1"
 mkswap $disk"2"
-mkfs.btrfs -f $disk"3"
-mkfs.btrfs -f $disk"4"
+mkfs.btrfs -f -d single $disk"3"
+mkfs.btrfs -f -d single $disk"4"
 #Subvolume creation
 mount $disk"3" /mnt
 btrfs sub cr /mnt/@
