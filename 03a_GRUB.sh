3152e4e4cba0f77e0dd4d88419c9fce89d3959c1
Łukasz Pękalski <lukasz.pekalski@outlook.com>
2024-01-27 19:27:33 +0100

    Update 03a_GRUB.sh

--- 03a_GRUB.sh
+++ 03a_GRUB.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+mkdir -p /boot/efi/EFI/BOOT
+cp /boot/efi/EFI/GRUB/grubx64.efi /boot/efi/EFI/BOOT/BOOTX64.EFI
+touch /boot/efi/startup.nsh
+echo 'bcf boot add 1 fs0:\EFI\GRUB\grubx64.efi "GRUB bootloader"' >> /boot/efi/startup.nsh
+echo 'exit' >> /boot/efi/startup.nsh
